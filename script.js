b00ced96cd2b85a442e6e17b550a1da2f1142127
mintway0341 <44641348+mintway0341@users.noreply.github.com>
2022-01-14 17:59:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function download_aos()
+{
+    window.open("https://play.google.com/store/apps/details?id=com.mintway_dev.theme.korea_university");
+}
+function download_ios()
+{
+    window.location.assign('Korea University.ktheme');
+}
+function visit_instagram()
+{
+    window.open("https://www.instagram.com/u_mini.341/");
+}
+
+function change_color_1_aos()
+{
+    document.getElementById("button_download_aos").style.backgroundColor = '#61011e';
+}
+function change_color_2_aos()
+{
+    document.getElementById("button_download_aos").style.backgroundColor = '#8B0029';
+}
+function change_color_1_ios()
+{
+    document.getElementById("button_download_ios").style.backgroundColor = '#61011e';
+}
+function change_color_2_ios()
+{
+    document.getElementById("button_download_ios").style.backgroundColor = '#8B0029';
+}
+function change_color_1_instagram()
+{
+    document.getElementById("button_instagram").style.backgroundColor = '#61011e';
+}
+function change_color_2_instagram()
+{
+    document.getElementById("button_instagram").style.backgroundColor = '#8B0029';
+}
